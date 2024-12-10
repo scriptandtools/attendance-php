baf11a6fc8b50842b761046eb3c252435cb2d11f
scriptandtools <scriptandtools1@gmail.com>
2024-12-10 12:17:15 +0500

    Update footer.php

--- ClassTeacher/Includes/footer.php
+++ ClassTeacher/Includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by NATIONAL DIPLOMA
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by Script and tools
               STUDENT ATTENDANCE MANAGEMENT SYSTEM
             </span>
           </div>
